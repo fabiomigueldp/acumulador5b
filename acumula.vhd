6341a78b1c9bd50cf17766c070e7f39b5669b598
Fábio Miguel <fabiomigueldp@gmail.com>
2025-06-23 20:04:03 +0000

    nr

--- acumula.vhd
+++ acumula.vhd
@@ -1,16 +1,6 @@
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.STD_LOGIC_ARITH.ALL;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
-
-ENTITY acumula IS
-   PORT (  C,L,T : IN  STD_LOGIC;                         -- Clear, Load, Transfer
-          D      : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);      -- dado de entrada
-          R      : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));     -- resultado acumulado
-END acumula;
-
 ARCHITECTURE func OF acumula IS
    SIGNAL B,A,S : STD_LOGIC_VECTOR(3 DOWNTO 0);
+   SIGNAL clrA  : STD_LOGIC;                     -- ❶ novo sinal
 
    COMPONENT reg4bits
       PORT ( clk  : IN  STD_LOGIC;
@@ -19,20 +9,23 @@ ARCHITECTURE func OF acumula IS
              Q    : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
    END COMPONENT;
 BEGIN
-   -- Registrador B (carrega o operando)
+   -- Conversão do Clear ativo-alto (C) para ativo-baixo do registrador
+   clrA <= NOT C;                                -- ❷ inversão fora do PORT MAP
+
+   -- Registrador B – LOAD
    RB : reg4bits
-      PORT MAP ( clk => L,            -- pulso LOAD
-                 clr => '1',          -- nunca é limpo
+      PORT MAP ( clk => L,
+                 clr => '1',
                  D   => D,
                  Q   => B );
 
-   -- Somador A + B
+   -- Somador
    S <= A + B;
 
-   -- Registrador A (armazena acumulação)
+   -- Registrador A – TRANSFER
    RA : reg4bits
-      PORT MAP ( clk => T,            -- pulso TRANSFER
-                 clr => NOT C,        -- CLEAR ativo-alto → ativo-baixo do reg
+      PORT MAP ( clk => T,
+                 clr => clrA,                    -- ❸ passa o sinal já invertido
                  D   => S,
                  Q   => A );
 
