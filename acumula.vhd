1dc17eb77caa9c6281f462994617fd259228125f
Fábio Miguel <fabiomigueldp@gmail.com>
2025-06-23 20:06:28 +0000

    nr2

--- acumula.vhd
+++ acumula.vhd
@@ -1,34 +1,46 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+ENTITY acumula IS
+   PORT ( C,L,T : IN  STD_LOGIC;                      -- Clear, Load, Transfer
+          D     : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);   -- dado de entrada
+          R     : OUT STD_LOGIC_VECTOR(3 DOWNTO 0) ); -- resultado
+END acumula;
+
 ARCHITECTURE func OF acumula IS
-   SIGNAL B,A,S : STD_LOGIC_VECTOR(3 DOWNTO 0);
-   SIGNAL clrA  : STD_LOGIC;                     -- ❶ novo sinal
+   SIGNAL B, A, S : STD_LOGIC_VECTOR(3 DOWNTO 0);
+   SIGNAL clrA    : STD_LOGIC;                        -- sinal auxiliar p/ CLR
 
    COMPONENT reg4bits
       PORT ( clk  : IN  STD_LOGIC;
              clr  : IN  STD_LOGIC;
              D    : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
-             Q    : OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+             Q    : OUT STD_LOGIC_VECTOR(3 DOWNTO 0) );
    END COMPONENT;
 BEGIN
-   -- Conversão do Clear ativo-alto (C) para ativo-baixo do registrador
-   clrA <= NOT C;                                -- ❷ inversão fora do PORT MAP
+   ------------------------------------------------------------------
+   -- conversão Clear ativo-alto (C) → ativo-baixo do registrador
+   ------------------------------------------------------------------
+   clrA <= NOT C;
 
-   -- Registrador B – LOAD
+   -- Registrador B (LOAD)
    RB : reg4bits
       PORT MAP ( clk => L,
-                 clr => '1',
+                 clr => '1',          -- nunca resetado
                  D   => D,
                  Q   => B );
 
    -- Somador
    S <= A + B;
 
-   -- Registrador A – TRANSFER
+   -- Registrador A (TRANSFERE resultado)
    RA : reg4bits
       PORT MAP ( clk => T,
-                 clr => clrA,                    -- ❸ passa o sinal já invertido
+                 clr => clrA,         -- clear na borda de C
                  D   => S,
                  Q   => A );
 
-   -- Saída
    R <= A;
 END func;
